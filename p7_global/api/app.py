46cdfb8dc3e16c8bfb9fdc01e2876bf79a538030
Pierre-Jordi Guettaf <pierrejordiguettaf@gmail.com>
2024-01-21 18:13:28 +0100

    Correction bug pred erronée via mise à jour process embedding specifique à l'embedding préentrainé

--- p7_global/api/app.py
+++ p7_global/api/app.py
@@ -3,14 +3,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from tensorflow import expand_dims
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-import json
-from tensorflow.keras.preprocessing.text import tokenizer_from_json
-
-# Opening JSON file
-with open('./p7_global/data/tokenizer_custom.json', 'r') as openfile:
-    json_object = json.load(openfile)
-    tokenizer_loaded = tokenizer_from_json(json_object)
+from keras.preprocessing.sequence import pad_sequences
 
 from p7_global.ml_logic.model import get_trained_model
 from p7_global.ml_logic.embedding import return_keyed_vectors, embed_sentence
@@ -30,10 +23,7 @@ app.add_middleware(
 # pour éviter des temps de chargements trop longs lors de la requete.
 # Idem pour le tokenizer custom pré-entrainé
 app.state.model = get_trained_model()
-
-with open('./p7_global/data/tokenizer_custom.json', 'r', encoding='utf-8') as openfile:
-    json_object = json.load(openfile)
-    app.state.tokenizer = tokenizer_from_json(json_object)
+app.state.choosen_wv = return_keyed_vectors()
 
 @app.get('/')
 def welcome_home():
@@ -50,16 +40,16 @@ def predict_single_tweet(text :str):
     """
     # Récupération du modèle pré-instancié
     model = app.state.model
-    # Récupération du dict d'embedding pré-instancié
-    tokenizer = app.state.tokenizer
+    # Récupération du dictionnaire d'embedding
+    wv = app.state.choosen_wv
     # Nettoyage et lemmatization de l'input
     text = cleaning_text_lemma(text)
     # Vectorisation du texte
-    text = tokenizer.texts_to_sequences([text])
-    # Padding du tenseur pour ramener la taille de nb_mots_vectorisé à 40
-    text = pad_sequences(text, dtype='float16', padding='post', maxlen=50)
-
-
+    text = embed_sentence(wv, text)
+    # Ajout d'une dimension pour correspondre au format attendu
+    text = expand_dims(text, axis=0)
+    # Padding pour correspondre au format attendu
+    text = pad_sequences(text, dtype='float16', padding='post', maxlen=40)
 
     try:
         # On réalise la prédiction sur le texte vectorisé
@@ -68,9 +58,9 @@ def predict_single_tweet(text :str):
         # Si on onbtient un score supérieur à 0.6 on considère le tweet
         # comme positif.
         if pred.numpy() > 0.6:
-            result = 'Positif'
-        else:
             result = 'Négatif'
+        else:
+            result = 'Positif'
     except Exception:
         result = "Prédiction erronnée"
 
